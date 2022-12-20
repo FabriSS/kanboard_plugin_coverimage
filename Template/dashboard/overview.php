ce8268dccd0e47621d83fe08438705a50334eb00
Fabrizio Puce <41839034+FabriSS@users.noreply.github.com>
2022-12-20 19:24:27 +0100

    Update overview.php
    this change adds the direct link to the project on the cover image

--- Template/dashboard/overview.php
+++ Template/dashboard/overview.php
@@ -3,7 +3,9 @@ $file = $this->task->projectCoverimageModel->getCoverimage($project['id']);
 if (isset($file)) {
 ?>
     <span class="avatar avatar-20 avatar-inline">
-        <img src="<?= $this->url->href('FileViewerController', 'thumbnail', array('file_id' => $file['id'], 'project_id' => $project['id'])) ?>" title="<?= $this->text->e($file['name']) ?>" alt="<?= $this->text->e($file['name']) ?>" vspace="5" hspace="3" height="25">
+        <a href="<?= 'https://garbit.it/kanboard/?controller=BoardViewController&action=show&project_id='.$project['id'] ?>" alt="<?= $this->text->e($file['name']) ?>">
+            <img src="<?= $this->url->href('FileViewerController', 'thumbnail', array('file_id' => $file['id'], 'project_id' => $project['id'])) ?>" title="<?= $this->text->e($file['name']) ?>" alt="<?= $this->text->e($file['name']) ?>" vspace="5" hspace="3" height="25">
+        </a>
     </span>
 <?php
 }
