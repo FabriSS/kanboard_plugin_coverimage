c19b85608530e975a263596bf2ab438e71081274
Fabrizio Puce <41839034+FabriSS@users.noreply.github.com>
2022-12-20 19:48:05 +0100

    Update overview.php

--- Template/dashboard/overview.php
+++ Template/dashboard/overview.php
@@ -3,7 +3,7 @@ $file = $this->task->projectCoverimageModel->getCoverimage($project['id']);
 if (isset($file)) {
 ?>
     <span class="avatar avatar-20 avatar-inline">
-        <a href="<?= 'https://garbit.it/kanboard/?controller=BoardViewController&action=show&project_id='.$project['id'] ?>" alt="<?= $this->text->e($file['name']) ?>">
+        <a href="<?= getenv('HTTP_HOST').'/?controller=BoardViewController&action=show&project_id='.$project['id'] ?>" alt="<?= $this->text->e($file['name']) ?>">
             <img src="<?= $this->url->href('FileViewerController', 'thumbnail', array('file_id' => $file['id'], 'project_id' => $project['id'])) ?>" title="<?= $this->text->e($file['name']) ?>" alt="<?= $this->text->e($file['name']) ?>" vspace="5" hspace="3" height="25">
         </a>
     </span>
